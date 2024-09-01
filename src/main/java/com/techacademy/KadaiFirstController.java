9401db0c5fc98079a2503547751716414a33bb74
KEISHIN-kira <ykira@lycorp.co.jp>
2024-09-01 20:34:36 +0900

    kadai-no-commit

--- src/main/java/com/techacademy/KadaiFirstController.java
+++ src/main/java/com/techacademy/KadaiFirstController.java
@@ -4,9 +4,11 @@ import java.util.Calendar;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/dayofweek")
 public class KadaiFirstController {
 	
 	@GetMapping("/{date}")
@@ -55,5 +57,31 @@ public class KadaiFirstController {
 
         return dayName;
 	}
+        
+        @GetMapping("/plus/{val1}/{val2}")
+        public String calcPlus(@PathVariable int val1, @PathVariable int val2) {
+            int res = 0;
+            res = val1 + val2;
+            return "計算結果：" + res;
+	}
+        @GetMapping("/minus/{val1}/{val2}")
+        public String calcMinus(@PathVariable int val1, @PathVariable int val2) {
+            int res = 0;
+            res = val1 - val2;
+            return "計算結果：" + res;
+        }
+        @GetMapping("/times/{val1}/{val2}")
+        public String calcTimes(@PathVariable int val1, @PathVariable int val2) {
+            int res = 0;
+            res = val1 * val2;
+            return "計算結果：" + res;
+   	
+            }
+        @GetMapping("/divide/{val1}/{val2}")
+        public String calcDivide(@PathVariable int val1, @PathVariable int val2) {
+            int res = 0;
+            res = val1 / val2;
+            return "計算結果：" + res;
 
+}
 }
